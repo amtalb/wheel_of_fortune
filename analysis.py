9b3776b5447b69b84d81b8d1554e2b0a10e1d9e7
amtalb <atalbott13@gmail.com>
2019-09-26 14:20:52 -0600

    messing with some stuff

--- analysis.py
+++ analysis.py
@@ -1,80 +1,44 @@
 import pandas
 import seaborn as sns
 import matplotlib.pyplot as plt
+from collections import Counter
 
-df = pandas.read_csv('data/clean_angelfire.csv')
+df = pandas.read_csv('./data/clean_angelfire.csv')
 
 def category_occurence():
-  print(df['Category'].value_counts())
+  count_list = df['Category'].value_counts()
+  series = pandas.Series(count_list)
+
+  return series
 
 def guessed_letter_occurence():
   letters = df['Letters'].str.split(" ", n=3, expand=True)
   letters = letters.dropna()
   print(letters.stack().value_counts())
 
-def puzzle_letter_occurence():  
+def puzzle_letter_occurence():
   puzzles = df['Puzzle']
-  print("A occurences: ", end='')
-  print(puzzles.str.count('A').sum())
-  print("B occurences: ", end='')
-  print(puzzles.str.count('B').sum())
-  print("C occurences: ", end='')
-  print(puzzles.str.count('C').sum())
-  print("D occurences: ", end='')
-  print(puzzles.str.count('D').sum())
-  print("E occurences: ", end='')
-  print(puzzles.str.count('E').sum())
-  print("F occurences: ", end='')
-  print(puzzles.str.count('F').sum())
-  print("G occurences: ", end='')
-  print(puzzles.str.count('G').sum())
-  print("H occurences: ", end='')
-  print(puzzles.str.count('H').sum())
-  print("I occurences: ", end='')
-  print(puzzles.str.count('I').sum())
-  print("J occurences: ", end='')
-  print(puzzles.str.count('J').sum())
-  print("K occurences: ", end='')
-  print(puzzles.str.count('K').sum())
-  print("L occurences: ", end='')
-  print(puzzles.str.count('L').sum())
-  print("M occurences: ", end='')
-  print(puzzles.str.count('M').sum())
-  print("N occurences: ", end='')
-  print(puzzles.str.count('N').sum())
-  print("O occurences: ", end='')
-  print(puzzles.str.count('O').sum())
-  print("P occurences: ", end='')
-  print(puzzles.str.count('P').sum())
-  print("Q occurences: ", end='')
-  print(puzzles.str.count('Q').sum())
-  print("R occurences: ", end='')
-  print(puzzles.str.count('R').sum())
-  print("S occurences: ", end='')
-  print(puzzles.str.count('S').sum())
-  print("T occurences: ", end='')
-  print(puzzles.str.count('T').sum())
-  print("U occurences: ", end='')
-  print(puzzles.str.count('U').sum())
-  print("V occurences: ", end='')
-  print(puzzles.str.count('V').sum())
-  print("W occurences: ", end='')
-  print(puzzles.str.count('W').sum())
-  print("X occurences: ", end='')
-  print(puzzles.str.count('X').sum())
-  print("Y occurences: ", end='')
-  print(puzzles.str.count('Y').sum())
-  print("Z occurences: ", end='')
-  print(puzzles.str.count('Z').sum())
+  alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  count_list = []
 
-# set the background colour of the plot to white
-sns.set(style="whitegrid", color_codes=True)
-# setting the plot size for all plots
-sns.set(rc={'figure.figsize':(11.7,8.27)})
-# create a countplot
-count_plot = sns.countplot(y='Category',data=df, order=df['Category'].value_counts().index)
-# Remove the top and down margin
-sns.despine(offset=10, trim=True)
-# display the plotplt.show()
+  for letter in alphabet:
+    count_list.append(puzzles.str.count(letter).sum())
 
-count_plot.figure.savefig("categories.png")
+  series = pandas.Series(count_list, index=alphabet)
+  return series
+
+def print_barh_graph(series,savename="image.png"):
+  series.plot.barh()
+  plt.gca().invert_yaxis()
+  plt.savefig(savename)
+
+for category in df['Category'].unique():
+  counter = Counter()
+  puzzles = df.loc[df['Category'] == category, 'Puzzle']
+  for row in puzzles:
+    counter.update(row)
+  count_list = list(counter.items())
+  count_list.sort()
+  if count_list[0][0] == " ":
+    del count_list[0]
+  print(count_list)
--- angelfire_cleaner.py
+++ angelfire_cleaner.py
@@ -4,7 +4,8 @@ with open('angelfire.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         new_file = open("clean_angelfire.csv", "a+")
-        row_str = ','.join(row[1:4])
-        if len(row_str) > 3:
-          new_file.write(row_str + '\n')
-          new_file.close()
+        if len(row) > 2 and len(row[3]) > 1:
+            row_str = ','.join(row[1:4])
+            if len(row_str) > 3:
+                new_file.write(row_str + '\n')
+                new_file.close()
